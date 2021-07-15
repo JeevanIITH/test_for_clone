cd0736dc2629c52a2f88c5d6ea950c09bfe3b8e7
JeevanIITH <cs20btech11047@iith.ac.in>
2021-07-15 07:00:23 +0530

    test message

--- main.cpp
+++ main.cpp
@@ -11,22 +11,5 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     soundclass *sd=sd->getinstance();
-    //D:/qt_projects/sdl_path_test/sounds/m1.mp3
-    /*
-    sd->addmusic("sounds1/m1.mp3");
-    sd->playmusic(0);
-    int i=0;
-
-    while((i!=-1))
-    {
-        std::cout<<"Enter input"<<std::endl;
-        std::cin>> i;
-    }
-    if(i==-1)
-    {
-        sd->delete_all();
-    }
-
-        */
     return a.exec();
 }
